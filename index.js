1be99724ce858c2f13a1e048b6ac0079f714e7f7
Vinicargui <vinicius.cardoso8888@outlook.com>
2023-05-28 00:08:14 -0300

    Alteração na config

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+require("dotenv").config();
+const bcript = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
 const mongoose = require("mongoose");
 const produtoRouter = require("./routes/produtoRoutes");
@@ -10,6 +13,7 @@ const router = require("./routes/route");
 const cors = require("cors");
 
 app.use(express.static("uploads"));
+
 //CONF PARA LER JSON
 app.use(
   express.urlencoded({
@@ -25,21 +29,18 @@ app.use("/", deleteProdutoRoutes);
 app.use("/", getProdutoRouter);
 app.use("/", updateProduto);
 app.use("/image", getImageRoutes);
+app.use(router); //rota usuarios
 
-app.use(router);
-
-const DB_USER = "vinicargui";
-const DB_PASSWORD = encodeURIComponent("nycJoXhJFbtBNWez");
-
+//coneção com o banco de dados
+const dbuser = process.env.DB_USER;
+const dbpass = process.env.DB_PASS;
 mongoose
   .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@menusystemcluster.dfxrsjq.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${dbuser}:${dbpass}@menusystemcluster.dfxrsjq.mongodb.net/?retryWrites=true&w=majority`
   )
-
   .then(() => {
     console.log("Conectamos com Sucessoooo!");
   });
-// .catch((err) => console.log(err));
 
 //PORTA DISPONIVEL
 app.listen(3001);
